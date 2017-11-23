f804e2338ec76e857fb26b92b1b0b666e612a15b
Takaaki NONAKA <takaakinonaka13@gmail.com>
2017-11-23 15:03:18 +0900

    modified sms2.py

--- sms2.py
+++ sms2.py
@@ -1,17 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 from flask_cors import CORS
 
 from db_setup import engine, Comics, ComicReviewNouns, ComicStoryNouns, ReviewNouns, StoryNouns
 from sqlalchemy.orm import sessionmaker
 
+from wordcloud import WordCloud
+
+import io
 import json
+import itertools
+import collections
 import numpy as np
 
 from my_module import file2object
 
 # consts and converters
 NETWORK_GRAPH = file2object('./misc/network_graph.json')
-COMICS_ARRAY = np.load('./misc/comics_array.npy')
+COMICS_ARRAY  = np.load('./misc/comics_array.npy')
 
 consts = file2object('./misc/consts.pickle')
 THEME17 = consts['theme17']
@@ -21,7 +26,20 @@ converter = file2object('./misc/cvt.pickle')
 num2theme    = converter['theme17']
 theme2num    = converter['theme17_']
 num2magazine = converter['magazine']
+magazine2num = converter['magazine_']
 num2genre    = converter['genre']
+genre2num    = converter['genre_']
+num2combination = converter['combination17']
+combination2num = converter['combination17_']
+
+# wordcloud settings
+font_path = './misc/ヒラギノ丸ゴ ProN W4.ttc'
+kwargs = {
+    'background_color': 'white',
+    'font_path': font_path,
+    'width': 1500,
+    'height': 1000
+}
 
 Session = sessionmaker(engine)
 session = Session()
@@ -45,6 +63,30 @@ def calculate_distance(title_id, array):
 	return distance_list
 
 
+def fit_to_c3(array, isCombination=False):
+	for m, r in enumerate(array):
+		if r.sum() != 0:
+			r = np.round(r / r.sum() * 100, 2)
+		array[m] = r
+
+	columns = []
+	if isCombination:
+		for m, r in enumerate(array.T):
+			lst = r.tolist()
+			combination = '&'.join(num2combination[m])
+			lst.insert(0, combination)
+			columns.append(lst)
+
+	else:
+		for m, r in enumerate(array.T):
+			lst = r.tolist()
+			theme = num2theme[m]
+			lst.insert(0, theme)
+			columns.append(lst)
+
+	return {'columns': columns}
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def dashboard(path):
@@ -63,22 +105,60 @@ def theme17():
 
 @app.route('/api/trend/genre/<genre>')
 def trend_genre(genre):
-	pass
+	genre_id = genre2num[genre]
+	array = np.zeros([17, 17])
+
+	for r in session.query(Comics.theme17).filter_by(genre_id=genre_id).all():
+		i = np.random.choice(range(0, 17))
+		for theme in r[0]:
+			j = theme2num[theme]
+			array[i][j] += 1
+
+	data = fit_to_c3(array)
+	return json.dumps(data, ensure_ascii=False)
 
 
 @app.route('/api/trend/magazine/<magazine>')
 def trend_magazine(magazine):
-	pass
+	magazine_id = magazine2num[magazine]
+	array = np.zeros([17, 17])
+
+	for r in session.query(Comics.theme17).filter_by(magazine_id=magazine_id).all():
+		i = np.random.choice(range(0, 17))
+		for theme in r[0]:
+			j = theme2num[theme]
+			array[i][j] += 1
+
+	data = fit_to_c3(array)
+	return json.dumps(data, ensure_ascii=False)
 
 
 @app.route('/api/trend/theme')
 def trend_theme():
-	pass
+	array = np.zeros([17, 17])
+
+	for r in session.query(Comics.theme17).all():
+		i = np.random.choice(range(0, 17))
+		for theme in r[0]:
+			j = theme2num[theme]
+			array[i][j] += 1
+
+	data = fit_to_c3(array)
+	return json.dumps(data, ensure_ascii=False)
 
 
 @app.route('/api/trend/theme_combination')
 def trend_theme_combination():
-	pass
+	array = np.zeros([17, 136])
+
+	for r in session.query(Comics.theme17).all():
+		i = np.random.choice(range(0, 17))
+		for cmb in itertools.combinations(r[0], 2):
+			j = combination2num[cmb]
+			array[i][j] += 1
+
+	data = fit_to_c3(array, isCombination=True)
+	return json.dumps(data, ensure_ascii=False)
 
 
 @app.route('/api/search/theme')
@@ -90,7 +170,8 @@ def search_by_theme():
 def search_by_title():
 	title = request.args.get('title')
 	titles = []
-	for r in session.query(Comics.title).filter(Comics.title.op('like')('%{}%'.format(title))).all():
+	for r in session.query(Comics.title)\
+			.filter(Comics.title.op('like')('%{}%'.format(title))).all():
 		titles.append(r[0])
 	return json.dumps(titles, ensure_ascii=False)
 
@@ -135,7 +216,9 @@ def word_cloud(title):
 		else:
 			words[w] = v
 
-	return json.dumps(words, ensure_ascii=False)
+	wc = WordCloud(**kwargs).generate_from_frequencies(words)
+	return 'hello world'
+	# return send_file()
 
 
 @app.route('/api/similar_comics/<title>')
